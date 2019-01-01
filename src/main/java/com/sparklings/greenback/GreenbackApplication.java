48398c40caf5ee1cacebb366211385d1c688574d
sdaftary <sdaftary@gmail.com>
2019-01-01 11:19:13 -0500

    Initianl

---
+++ src/main/java/com/sparklings/greenback/GreenbackApplication.java
@@ -0,0 +1,14 @@
+package com.sparklings.greenback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenbackApplication.class, args);
+	}
+
+}
+
